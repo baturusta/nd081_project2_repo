75edf6de229ce72f37d8ce62dc21431baf173d9e
Batur Usta <batur.usta93@gmail.com>
2021-06-24 12:14:10 +0300

    Update __init__.py

--- NeighborlyAPI/createAdvertisement/__init__.py
+++ NeighborlyAPI/createAdvertisement/__init__.py
@@ -5,19 +5,7 @@ import json
 def main(req: func.HttpRequest, sendGridMessage: func.Out[str]) -> func.HttpResponse:
 
     request = req.get_json()
-    value = "Sending message from my Azure Functions HTTP Trigger"
-
-    message = {
-        "personalizations": [ {
-          "to": [{
-            "email": "batur.usta93@gmail.com"
-            }]}],
-        "subject": "[AZURE FUNCTIONS SENDGRID] email",
-        "content": [{
-            "type": "text/plain",
-            "value": value }]}
-
-    sendGridMessage.set(json.dumps(message))
+    
     if request:
         try:
             url = "mongodb://project2cosmosdb:oIEHxvzEXWWZIugAqL74I0qJIgrc01qSmydEfw8T5hYvVRLjJ2cemDhpnd0RXYjapnLYb7iK5LWf4nsvxWx92w==@project2cosmosdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@project2cosmosdb@"  # TODO: Update with appropriate MongoDB connection information
@@ -26,6 +14,20 @@ def main(req: func.HttpRequest, sendGridMessage: func.Out[str]) -> func.HttpResp
             collection = database['advertisements']
 
             rec_id1 = collection.insert_one(eval(request))
+
+            value = "Sending message from my Azure Functions HTTP Trigger"
+
+            message = {
+                        "personalizations": [ {
+                        "to": [{
+                    "email": "batur.usta93@gmail.com"
+                        }]}],
+                    "subject": "[AZURE FUNCTIONS SENDGRID] email",
+                    "content": [{
+                        "type": "text/plain",
+                        "value": value }]}
+
+            sendGridMessage.set(json.dumps(message))
 
             return func.HttpResponse(req.get_body())
 
